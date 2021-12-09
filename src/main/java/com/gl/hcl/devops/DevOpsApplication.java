5e4cf62523b53b9b449c2b18cf384aed7e5f60ba
HARIRAMAKRRISHNAN R <hariramakrrishnan.r@hcl.com>
2021-12-09 23:04:09 +0530

    first commit

---
+++ src/main/java/com/gl/hcl/devops/DevOpsApplication.java
@@ -0,0 +1,22 @@
+package com.gl.hcl.devops;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevOpsApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevOpsApplication.class, args);
+		//System.out.println("Hello World");
+		System.out.println("Hello Dev-Ops");
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
